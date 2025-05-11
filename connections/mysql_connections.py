48503208d89fcea2fb693ca76bd1b4b0b36eed55
John Sanchez <jrsanchezr8906@gmail.com>
2025-05-10 19:57:30 -0500

    actualizacion de .py

--- connections/mysql_connections.py
+++ connections/mysql_connections.py
@@ -6,5 +6,5 @@ PASSWORD = 'Compaq*87'
 #engine_raw_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@localhost:3306/RAW_DATA')
 #engine_clean_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@localhost:3306/CLEAN_DATA')
 
-engine_raw_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@mysql:3306/RAW_DATA')
-engine_clean_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@mysql:3306/CLEAN_DATA')
+engine_raw_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@localhost:8082/RAW_DATA')
+engine_clean_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@localhost:8082/CLEAN_DATA')
